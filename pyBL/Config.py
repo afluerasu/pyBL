0c386f7aebee9cbbe0500e1d62a539f17930f3ba
Arman Arkilic <arkilic@box32-2.cs.nsls2.local>
2013-08-23 15:20:02 -0400

    PV import added to configuration

--- pyBL/Config.py
+++ pyBL/Config.py
@@ -4,21 +4,30 @@ Created on Aug 9, 2013
 @author: arkilic
 '''
 from pyBL._conf import _confBL
+from pyOlog._conf import _conf
+
 
 NAME=_confBL.get('diffractometer_config', 'name')
 GEOMETRY=_confBL.get('diffractometer_config','geometry')
 ENGINE=_confBL.get('diffractometer_config','engine')
 TAG=_confBL.get('diffractometer_config','tag')
 AUTHOR=_confBL.get('diffractometer_config','author')
+pv1=_confBL.get('diffractometer_config','pv1')
+pv2=_confBL.get('diffractometer_config','pv2')
+pv3=_confBL.get('diffractometer_config','pv3')
+pv4=_confBL.get('diffractometer_config','pv4')
+pv5=_confBL.get('diffractometer_config','pv5')
+pv6=_confBL.get('diffractometer_config','pv6')
+pvList=[pv1,pv2,pv3,pv4,pv5,pv6]
 
 def config(name,geometry,engine,tag,author):    
     from Diffractometer import Diffractometer    
-    from os import environ
-    from os.path import expanduser
     diff=Diffractometer(name, geometry, engine, tag, author)
     diff.dummySetup(name, geometry, engine, tag, author)
+    i=0
+    for angle in diff.getangleList():
+        angle.setPV(pvList[i])
+        i+=1
     return diff
 
-
 diff=config(name=NAME, geometry=GEOMETRY, engine=ENGINE, tag=TAG, author=AUTHOR)
-
