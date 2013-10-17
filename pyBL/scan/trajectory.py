6b29c546125eaae56cda0d09828681111f2c0ffe
Arman Arkilic <arkilic@box32-2.cs.nsls2.local>
2013-10-17 16:16:55 -0400

    Trajectory generation for given hkl positions

--- pyBL/scan/trajectory.py
+++ pyBL/scan/trajectory.py
@@ -3,54 +3,76 @@ Created on Oct 9, 2013
 
 @author: arkilic
 '''
-#from Config import diff
 from commands import *
+
 def genTraj(hCoordinates,kCoordinates,lCoordinates,resolution ):
+    '''
+    Returns [(motor positions for given hkl),{azimuthal angles}] and corresponding hkl values
+    '''
     temp=list()
-    maximum=max((hCoordinates[-1]-hCoordinates[0])/resolution,
-                (kCoordinates[-1]-kCoordinates[0])/resolution,
-                (lCoordinates[-1]-lCoordinates[0])/resolution)
+    lists=dict()
+    hSteps=dict()
+    kSteps=dict()
+    lSteps=dict()
+    maxStepSize=0
+    if type(hCoordinates) is list:
+        lists['h']=hCoordinates
+    else:
+        lists['h']=[hCoordinates]
+    if type(kCoordinates) is list:
+        lists['k']=kCoordinates
+    else:
+        lists['k']=[kCoordinates]
+    if type(lCoordinates) is list:
+        lists['l']=kCoordinates
+    else:
+        lists['l']=[lCoordinates]
+    h=lists['h']
+    k=lists['k']
+    l=lists['l']
+    maxStepSize=max((h[-1]-h[0])/resolution,
+                (k[-1]-k[0])/resolution,
+                (l[-1]-l[0])/resolution)
     if getUBFlag()==False:
         raise Warning('No UB Calculation in progress. Please use newub() or loadub()')
     else:
+        print 'else',maxStepSize
         if getRefFlag()==True:
-            blankList=[0]*len(maximum)
-            
-            if hCoordinates!=0:
+            if type(hCoordinates) is list:
                 hSteps=detSingleInterval(coordinateName='h', initial=hCoordinates[0], final=hCoordinates[-1], step=resolution)
             else:
-                hSteps={'h':blankList}
-                
-            if kCoordinates!=0:
+                h_temp=lists['h']*int((round(maxStepSize)+1))
+                hSteps={'h':h_temp}            
+            if type(kCoordinates) is list:
                 kSteps=detSingleInterval(coordinateName='k', initial=kCoordinates[0], final=kCoordinates[-1], step=resolution)
             else:
-                kSteps={'k':blankList}
-                
-            if lCoordinates!=0:
+                k_temp=lists['k']*int((round(maxStepSize)+1))
+                kSteps={'k':k_temp}
+            if type(lCoordinates) is list:
                 lSteps=detSingleInterval(coordinateName='l', initial=lCoordinates[0], final=lCoordinates[-1], step=resolution)
             else:
-                lSteps={'l':blankList}
+                l_temp=lists['l']*int((round(maxStepSize)+1))
+                lSteps={'l':l_temp}
             steps=dict(hSteps.items()+kSteps.items()+lSteps.items())
-            print steps
             i=0
             while i<len(steps['h']):
-                print steps['h'][i]
                 temp.append(hkl_to_angles(h=steps['h'][i], k=steps['k'][i], l=steps['l'][i], energy=energy()))
                 i+=1
+                if len(steps['h'])!=len(steps['k']) or len(steps['k'])!=len(steps['l']):
+                    raise Exception('Please enter identical intervals')
         else:
             raise Warning('No reflection added.Please use addref() to add a reflection for your crystal')
+    return temp,steps
 
 def detSingleInterval(coordinateName,initial,final,step):
-    currentStep=initial
-    
+    currentStep=float(initial)
     trajList={coordinateName:[]}
-    while currentStep<=final:
+    trajList[coordinateName].append(currentStep)
+    while currentStep<final:
         currentStep=float(currentStep)+float(step)
         trajList[coordinateName].append(currentStep)
     return trajList
 
-def detMultTraj(coordinateNames):
-    pass
 def checkTraj(trajectoryList):
     pass
 
@@ -71,5 +93,6 @@ con('mu',0)
 con()
 set_low_limit('delta',0)
 hkl_to_angles(1,0,0,energy())
+genTraj(hCoordinates=[1,1.3],kCoordinates=0,lCoordinates=0,resolution=0.1)
 ls
 '''
